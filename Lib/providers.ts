9ce1b8ced1cb0b7569eb990e5f8c38fdbe7a027c
UtekosKristoffer <santini91yt@gmail.com>
2025-08-20 10:47:56 +0200

    Create providers.ts

---
+++ Lib/providers.ts
@@ -0,0 +1,17 @@
+// src/Components/Providers.tsx
+'use client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import getQueryClient from '../lib/helpers/getQueryClient';
+import { CartProcessContext } from './cart/CartProcessClient';
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+  return (
+    <CartProcessContext.Provider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </CartProcessContext.Provider>
+  );
+}
