64630f63e8161f538b3dbf7e02574b941bd01422
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-09-16 16:08:00 +0200

    update

--- src/hooks/useSelectedVariant.ts
+++ src/hooks/useSelectedVariant.ts
@@ -6,6 +6,6 @@ export function useSelectedVariant(product: ShopifyProduct) {
   const searchParams = useSearchParams()
 
   const variantId = searchParams.get('variant')
-  const allVariants = product.variants.edges.map(e => e.nodes)
+  const allVariants = product.variants.edges.map(e => e.node)
   return allVariants.find(v => v.id === variantId) || allVariants[0]
 }
