014049eb40b7bb210dda4be618e8e1305fcce4b0
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-09-16 14:57:09 +0200

    update

--- src/app/produkter/[handle]/ProductPageView/types.ts
+++ src/app/produkter/[handle]/ProductPageView/types.ts
@@ -1,13 +1,9 @@
-import type {
-  ShopifyImage,
-  ShopifyProduct,
-  ShopifyProductVariant
-} from '@types'
+import type { Image, ShopifyProduct, ShopifyProductVariant } from '@types'
 
 export type ProductPageViewProps = {
   productData: ShopifyProduct
   selectedVariant: ShopifyProductVariant
   allVariants: ShopifyProductVariant[]
-  variantImages: ShopifyImage[]
+  variantImages: Image[]
   onOptionChange: (_optionName: string, _value: string) => void
 }
