7ee33e17c135587f97ba3e340369fc0722a7996e
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-11-05 13:33:03 +0100

    l

--- src/app/gaveguide/farsdag/page.tsx
+++ src/app/gaveguide/farsdag/page.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     siteName: 'Utekos',
     images: [
       {
-        url: '/og-image-farsdag.png',
+        url: '/Farsdag2025.png',
         width: 1200,
         height: 630,
         alt: 'En perfekt farsdagsgave.'
