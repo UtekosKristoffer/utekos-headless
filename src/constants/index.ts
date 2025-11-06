accbfc9bcc97df69635172ac5016741a9ae32816
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-11-06 13:16:13 +0100

    l

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const SHOPIFY_ADMIN_ACCESS_TOKEN =
-  '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'
-export const SHOPIFY_STORE_DOMAIN = 'erling-7921.myshopify.com'
+  process.env.SHOPIFY_ADMIN_ACCESS_TOKEN!
+export const SHOPIFY_STORE_DOMAIN = process.env.SHOPIFY_STORE_DOMAIN!
