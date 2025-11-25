9cf2513c815bfd76dda9faeaefd254056ddb24c9
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-11-25 15:01:04 +0100

    l

--- src/lib/errors/CartNotFoundError.ts
+++ src/lib/errors/CartNotFoundError.ts
@@ -1,17 +1,4 @@
 // Path: src/lib/errors/CartNotFoundError.ts
-
-/**
- * @module lib/errors
- */
-
-/**
- * Custom error class to represent a "Cart Not Found" error.
- * Exist because carts can be deleted or expire, and we want to handle that case specifically.
- * @class CartNotFoundError
- * @extends Error
- * @author Hjelmeland
- */
-
 export class CartNotFoundError extends Error {
   constructor(message: string) {
     super(message)
