f805abb82c69bcbb60c5b337b969207b9eb2a449
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-12-19 12:09:46 +0100

    l

--- src/components/analytics/Klaviyo/trackAddedToCart.tsx
+++ src/components/analytics/Klaviyo/trackAddedToCart.tsx
@@ -1,41 +1,41 @@
 import { getMarketingParams } from './getMarketingParams'
-import type { ShopifyProduct } from '@types' // Antar at denne typen er definert i prosjektet ditt
+import type { ShopifyProduct } from '@types'
 import { getOrSetExternalId } from '../MetaPixel/getOrSetExternalId'
-import { track } from '@vercel/analytics'
+import { track } from '@vercel/analytics/react' // Husk /react for klientside tracking
 
 export function trackAddedToCart(product: ShopifyProduct) {
-  if (window.klaviyo) {
-    const marketingParams = getMarketingParams()
-    const variant = product.selectedOrFirstAvailableVariant
-    const trackAtc = track('Added to Cart', {
-      product: product.title,
-      productId: product.id,
-      variantId: variant.id,
-      quantity: 1,
-      price: parseFloat(variant.price.amount)
-    })
-    const item = {
-      Name: product.title,
-      ProductID: product.id.substring(product.id.lastIndexOf('/') + 1),
-      ImageURL: variant.image?.url || '',
-      Handle: product.handle,
+  if (typeof window === 'undefined') return
+
+  const marketingParams = getMarketingParams()
+  const variant = product.selectedOrFirstAvailableVariant
+
+  track('Added to Cart', {
+    product: product.title,
+    productId: product.id,
+    variantId: variant.id,
+    quantity: 1,
+    price: parseFloat(variant.price.amount)
+  })
+
+  const item = {
+    Name: product.title,
+    ProductID: product.id.substring(product.id.lastIndexOf('/') + 1),
+    ImageURL: variant.image?.url || '',
+    Handle: product.handle,
+    Brand: product.vendor,
+    Price: variant.price.amount,
+    ProductVariantID: variant.id,
+    Value: variant.price.amount,
+    Customer: getOrSetExternalId(),
+    Metadata: {
       Brand: product.vendor,
       Price: variant.price.amount,
-      ProductVariantID: variant.id,
-      Value: variant.price.amount,
-      Customer: getOrSetExternalId(), // Henter ekstern ID for sporing
-      Metadata: {
-        Brand: product.vendor,
-        Price: variant.price.amount,
-        CompareAtPrice: variant.compareAtPrice,
-        Source: marketingParams.source,
-        Medium: marketingParams.medium,
-        CampaignID: marketingParams.campaign_id
-      }
+      CompareAtPrice: variant.compareAtPrice,
+      Source: marketingParams.source,
+      Medium: marketingParams.medium,
+      CampaignID: marketingParams.campaign_id
     }
-
-    window.klaviyo.track('Viewed Product', item)
-    window.klaviyo.trackViewedItem(item)
-    track
   }
+
+  window.klaviyo.track('Added to Cart', item)
 }
