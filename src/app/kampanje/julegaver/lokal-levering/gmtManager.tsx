c33e37a73bc2caad80e7a0349783b9ef02824b40
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-12-20 21:25:58 +0100

    l

--- src/app/kampanje/julegaver/lokal-levering/gmtManager.tsx
+++ src/app/kampanje/julegaver/lokal-levering/gmtManager.tsx
@@ -1,5 +1,17 @@
-import { GoogleTagManager } from '@next/third-parties/google'
+'use client'
 
-export default function Page() {
-  return <GoogleTagManager gtmId='GTM-XYZ' />
+import { useEffect } from 'react'
+import { sendGTMEvent } from '@next/third-parties/google'
+export function ConversionTracker() {
+  useEffect(() => {
+    sendGTMEvent({
+      event: 'conversion',
+      conversion_label: 'GgpCLfd09QbEPqM_7BC',
+      value: 'DATA_HER',
+      transaction_id: '!7708188692',
+      content_id: 'AW-17708188692'
+    })
+  }, []) // De tomme klammene []
+
+  return null
 }
--- src/app/kampanje/julegaver/lokal-levering/layout.tsx
+++ src/app/kampanje/julegaver/lokal-levering/layout.tsx
@@ -1,7 +1,10 @@
 //Path: src/app/kampanje/julegaver/lokal-levering/layout.tsx
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import { BergenDeliveryJsonLd } from './BergenDeliveryJsonLd'
 import type { ReactNode } from 'react'
-import { GoogleTagLocalDelivery } from './GoogleTag'
+import { GoogleAdsConversion } from './GoogleAdsConversion'
+import { sendGTMEvent } from '@next/third-parties/google'
 
 export default function BergenDeliveryLayout({
   children
@@ -11,8 +14,9 @@ export default function BergenDeliveryLayout({
   return (
     <>
       <BergenDeliveryJsonLd />
+      <GoogleAnalytics gaId='AW-17819485818' />
       {children}
-      <GoogleTagLocalDelivery />
+      <GoogleAdsConversion />
     </>
   )
 }
