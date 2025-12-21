178e6207655ce8ab0552ddee080c747507475406
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-12-21 20:49:44 +0100

    l

--- src/components/frontpage/components/HeroSection/MotionContent.tsx
+++ src/components/frontpage/components/HeroSection/MotionContent.tsx
@@ -33,7 +33,7 @@ export function MotionContent() {
         className='animate-fade-in-up mx-auto mt-6 mb-12 max-w-2xl text-lg leading-relaxed text-foreground/80 md:max-w-4xl lg:text-2xl'
         style={{ animationDelay: '0.5s' }}
       >
-        Kompromissløs komfort. Overlegen allsidiget. Juster, form og nyt.
+        Kompromissløs komfort. Overlegen allsidighet. Juster, form og nyt.
       </p>
       <div
         className='animate-fade-in hidden justify-center md:flex'
