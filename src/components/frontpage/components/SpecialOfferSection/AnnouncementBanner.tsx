ffeb79d453b9fb7843ccecd8be43a54ce551525a
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2026-01-29 23:53:44 +0100

    l

--- src/components/frontpage/components/SpecialOfferSection/AnnouncementBanner.tsx
+++ src/components/frontpage/components/SpecialOfferSection/AnnouncementBanner.tsx
@@ -65,7 +65,7 @@ export default function AnnouncementBanner() {
             </span>
 
             <span className='inline md:hidden'>
-              Siste sjanse: Comfyrobe™ til kun kr 990,-
+              Siste sjanse: Comfyrobe™ kun kr 990,-
             </span>
 
             <span className='hidden md:inline'>
