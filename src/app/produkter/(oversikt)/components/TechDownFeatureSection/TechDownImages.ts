f4195df653fd3787890d3e6f44efbc1951c36a9b
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2026-02-03 04:19:56 +0100

    l

--- src/app/produkter/(oversikt)/components/TechDownFeatureSection/TechDownImages.ts
+++ src/app/produkter/(oversikt)/components/TechDownFeatureSection/TechDownImages.ts
@@ -1,10 +1,15 @@
 import TechDownImageFront from '@public/1080/utekos-techdown.png'
 import TechDownImagePatch from '@public/1080/tech-bakside-hel-1080.png'
+import TechDownImage from '@public/1080/techdown.png'
 
 export const TechDownImages = [
   { src: TechDownImageFront, alt: 'Utekos TechDown komfortplagg sett forfra.' },
   {
     src: TechDownImagePatch,
     alt: 'Nærbilde av det slitesterke Luméa-stoffet på Utekos TechDown.'
+  },
+  {
+    src: TechDownImage,
+    alt: 'Utekos TechDown komfortplagg sett forfra.'
   }
 ]
