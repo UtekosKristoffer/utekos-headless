98e1022ad351acc2131e42eeba63b8affd984717
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2026-02-07 18:07:16 +0100

    jo

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -116,6 +116,7 @@ export function ProductCard({
               src={imageUrl}
               alt={altText}
               fill
+              quality={100}
               sizes='(min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw'
               className='object-cover transition-transform duration-300 group-hover:scale-105'
               priority={isPriority}
