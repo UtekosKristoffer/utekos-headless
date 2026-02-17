9c7834c11754deafe13d6636d46bdb52cbaae84c
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2026-02-17 20:59:34 +0100

    wow

--- src/components/cart/CartFooter/utils/shouldRenderFooter.ts
+++ src/components/cart/CartFooter/utils/shouldRenderFooter.ts
@@ -1,4 +1,4 @@
-import type { Cart } from '@types'
+import type { Cart } from 'types/cart'
 
 export const shouldRenderFooter = (cart: Cart | null | undefined): boolean =>
   Boolean(cart && cart.lines.length > 0)
