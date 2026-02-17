b75448142a3beb1cdd611ded6993474ace37e188
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2026-02-17 21:26:49 +0100

    poiuyt

--- src/lib/errors/formatShopifyErrorResponse.ts
+++ src/lib/errors/formatShopifyErrorResponse.ts
@@ -1,18 +1,9 @@
 // Path: src/lib/errors/formatShopifyErrorResponse.ts
 
-/**
- * @fileoverview Formats Shopify GraphQL @param {ResponseErrors} ResponseErrors into @param {CartActionsResult} CartActionsResult.
- */
-
 import { CartErrorCode } from '@/constants/CartErrorCode'
 import type { ResponseErrors } from '@shopify/graphql-client'
-import type { CartActionsResult } from '@types'
+import type { CartActionsResult } from 'types/cart/CartActions'
 
-/**
- * @function formatShopifyErrorResponse
- * @param {ResponseErrors} errors - The error response from Shopify GraphQL API.
- * @returns {CartActionsResult} Standardized cart action result with error details.
- */
 export function formatShopifyErrorResponse(
   errors: ResponseErrors
 ): CartActionsResult {
