491eb5c01c12246df7d2827e2463c602cb5325db
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2026-02-17 21:04:03 +0100

    jo

--- src/hooks/createSwatchColorMap.ts
+++ src/hooks/createSwatchColorMap.ts
@@ -1,4 +1,4 @@
-import type { ShopifyProduct } from '@types'
+import type { ShopifyProduct } from 'types/product/ShopifyProduct'
 
 export function createSwatchColorMap(
   product: ShopifyProduct | undefined
