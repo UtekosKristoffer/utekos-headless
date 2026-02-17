546b836ddd880619332faed8bf3254852e3ced37
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2026-02-17 21:12:59 +0100

    cart

--- src/hooks/useCartOpen.ts
+++ src/hooks/useCartOpen.ts
@@ -1,7 +1,7 @@
 import { cartStore } from '@/lib/state/cartStore'
 import { useSelector } from '@xstate/store/react'
 
-import type { CartUserInterfaceContext } from '@types'
+import type { CartUserInterfaceContext } from 'types/cart'
 import type { StoreSnapshot } from '@xstate/store'
 
 export const selectIsOpen = (
