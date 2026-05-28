d07cb90d0afaaef2ec5a0d7f05a96dd182406822
Kristoffer Ohnstad Hjelmeland <kristoffer.hjelmeland@gmail.com>
2026-05-28 18:23:37 +0200

    jo

--- src/lib/tracking/utils/extractEventCookies.ts
+++ src/lib/tracking/utils/extractEventCookies.ts
@@ -1,16 +1,12 @@
 import type { EventCookies } from 'types/tracking/event/cookies/EventCookies'
 
-export function extractEventCookies(
-  cookies: Map<string, string>
-): EventCookies {
+export function extractEventCookies(cookies: Map<string, string>): EventCookies {
   return {
     fbp: cookies.get('_fbp'),
     fbc: cookies.get('_fbc'),
     externalId: cookies.get('ute_ext_id'),
     userHash: cookies.get('ute_user_hash'),
     scCid: cookies.get('ute_sc_cid'),
-    epik: cookies.get('_epik'),
-    ttclid: cookies.get('ute_ttclid'),
-    ttp: cookies.get('_ttp')
+    epik: cookies.get('_epik')
   }
 }
