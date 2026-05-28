02d83f0aa822ae0a9419c4c7adc7fd6268e29b17
Kristoffer Ohnstad Hjelmeland <kristoffer.hjelmeland@gmail.com>
2026-05-28 18:15:35 +0200

    jo

--- src/lib/tracking/utils/dispatchSecondaryEvents.ts
+++ src/lib/tracking/utils/dispatchSecondaryEvents.ts
@@ -1,14 +1,6 @@
-import { sendSnapPurchase } from '@/lib/tracking/snapchat/sendSnapPurchase'
 import { sendPinterestPurchase } from '@/lib/tracking/pinterest/sendPinterestPurchase'
-import { sendTikTokPurchase } from '@/lib/tracking/tiktok/sendTikTokPurchase'
 import type { TrackingContext } from 'types/tracking/user/TrackingContext'
 
-export async function dispatchSecondaryEvents(
-  context: TrackingContext
-): Promise<void> {
-  await Promise.all([
-    sendSnapPurchase(context),
-    sendPinterestPurchase(context),
-    sendTikTokPurchase(context)
-  ])
+export async function dispatchSecondaryEvents(context: TrackingContext): Promise<void> {
+  await Promise.all([sendPinterestPurchase(context)])
 }
